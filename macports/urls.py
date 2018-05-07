c782ccf2214d23ca106ee419e77e5d9697c9cbdf
vishnum <vishnum1998@gmail.com>
2018-05-08 05:15:33 +0530

    removed debug toolbar

--- macports/urls.py
+++ macports/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.urls import path
-from django.conf import settings
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -21,8 +21,3 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns = [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
