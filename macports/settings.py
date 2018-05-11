e623c9c60c29b1f5cb6d662027731a6803658a8f
vishnum <vishnu@macports.org>
2018-05-11 06:50:13 +0530

    Implement static files for django

--- macports/settings.py
+++ macports/settings.py
@@ -128,8 +128,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 django_heroku.settings(locals())
